80371720abff688aebc17c0237d66910c302ab25
Lucas Salazar <lucassalazar741@gmail.com>
2020-07-29 15:12:02 -0300

    Update new_task_dialog.py

--- new_task_dialog.py
+++ new_task_dialog.py
@@ -6,14 +6,6 @@ from tkinter import *
 from tkinter.ttk import Combobox
 
 
-class DialogMainFrame(Frame):
-    def __init__(self, master, **kw):
-        super().__init__(master, **kw)
-        self.grid_columnconfigure(1, weight=1)
-        DialogTopBar(self).grid(column=1, row=1, sticky='nwse')
-        DialogEntry(self).grid(column=1, row=2, sticky='nwse')
-
-
 # Widgets for the Top bar of the dialog
 class GoBack(Button):
     def __init__(self, master, **kw):
@@ -37,9 +29,14 @@ class DialogTopBar(Frame):
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
         self.grid_columnconfigure(2, weight=1)
-        GoBack(self).grid(column=1, row=1)
-        Title(self).grid(column=2, row=1)
-        SaveTask(self).grid(column=3, row=1)
+
+        self.go_back = GoBack(self)
+        self.title = Title(self)
+        self.save_task = SaveTask(self)
+
+        self.go_back.grid(column=1, row=1)
+        self.title.grid(column=2, row=1)
+        self.save_task.grid(column=3, row=1)
 
 
 # Widgets for the dialog entry frame
@@ -47,8 +44,14 @@ class TaskEntry(Frame):
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
         self.grid_columnconfigure(1, weight=1)
-        Label(self, text='What is to be done?').grid(column=1, row=1, sticky=W)
-        Entry(self).grid(column=1, row=2, sticky='nwse')
+
+        self.new_task_string = StringVar()
+
+        self.label = Label(self, text='What is to be done?')
+        self.entry = Entry(self, textvariable=self.new_task_string)
+
+        self.label.grid(column=1, row=1, sticky=W)
+        self.entry.grid(column=1, row=2, sticky='nwse')
 
 
 class Date(Frame):
@@ -71,6 +74,27 @@ class DialogEntry(Frame):
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
         self.grid_columnconfigure(1, weight=1)
-        TaskEntry(self).grid(column=1, row=1, sticky='nwse')
-        Date(self).grid(column=1, row=2, sticky='nwse')
-        TaskLists(self).grid(column=1, row=3, sticky='nwse')
+
+        self.task_entry = TaskEntry(self).grid(column=1, row=1, sticky='nwse')
+        self.date = Date(self).grid(column=1, row=2, sticky='nwse')
+        self.task_lists = TaskLists(self).grid(column=1, row=3, sticky='nwse')
+
+
+class DialogMainFrame(Frame):
+    def __init__(self, master, **kw):
+        super().__init__(master, **kw)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.dialog_top_bar = DialogTopBar(self)
+        self.dialog_entry = DialogEntry(self)
+
+        self.dialog_top_bar.grid(column=1, row=1, sticky='nwse')
+        self.dialog_entry.grid(column=1, row=2, sticky='nwse')
+
+
+class TaskWidget(Frame):
+    def __init__(self, master, txt, **kw):
+        super().__init__(master, **kw)
+        Checkbutton(self, text=txt, variable=self.info).grid(column=1, row=1, sticky=W)
+
+
