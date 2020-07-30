3fa81b9861cf812fe95a3a4256217b78c9bbc204
Lucas Salazar <lucassalazar741@gmail.com>
2020-07-30 17:03:38 -0300

    Update mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -5,7 +5,16 @@
 # - Entry to add quick tasks.
 from tkinter import *
 from tkinter import ttk
-from new_task_dialog import DialogMainFrame, TaskWidget
+import new_task_dialog
+
+
+class Dialog(new_task_dialog.DialogMainFrame):
+    def __init__(self, master, **kw):
+        super().__init__(master, **kw)
+        self.winfo_children()[0].winfo_children()[2].configure(command=lambda *args: (self.new_task_button(), self.destroy()))
+
+    def new_task_button(self):
+        new_task_dialog.TaskWidget(mainframe.task_grid, self.winfo_children()[1].new_task_string.get()).grid(sticky='nwse')
 
 
 # Main Tk window
@@ -15,13 +24,8 @@ class MainWindow(Tk):
         self.title('To do List')
         self.geometry('295x450')
         self.resizable(0, 0)
-        # self.configure(background='#222222')
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-
-    def call_new_task_dialog(self):
-        dialog = DialogMainFrame(self)
-        dialog.grid(column=0, row=0, sticky='nwse')
 
 
 class ListsCombobox(ttk.Combobox):
@@ -38,7 +42,7 @@ class Search(Entry):
 class NewTaskButton(Button):
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
-        self.configure(text='+', command=root.call_new_task_dialog)
+        self.configure(text='+', command=lambda *args: Dialog(root).grid(column=0, row=0, sticky='nwse'))
 
 
 class QuickTask(Entry):
@@ -49,6 +53,7 @@ class QuickTask(Entry):
 
         self.bind('<FocusIn>', lambda *args: self.delete(0, END))
         self.bind('<FocusOut>', lambda *args: self.insert(0, 'Enter Quick Task Here'))
+        self.bind('<Return>', add_quick_task)
 
 
 class TaskGrid(Frame):
@@ -95,14 +100,13 @@ class MainWindowMainframe(Frame):
 
 
 def add_quick_task(*args):
-    TaskWidget(mainframe.task_grid, mainframe.bottom_bar.quick_task_entry.task_string.get()).grid(sticky='nwse')
-    mainframe.task_grid, mainframe.bottom_bar.quick_task_entry.delete(0, END)
+    new_task_dialog.TaskWidget(mainframe.task_grid, mainframe.bottom_bar.quick_task_entry.task_string.get()).grid(sticky='nwse')
+    mainframe.bottom_bar.quick_task_entry.delete(0, END)
 
 
 root = MainWindow()
 
 mainframe = MainWindowMainframe(root)
 mainframe.grid(column=0, row=0, sticky='nwse')
-mainframe.bottom_bar.quick_task_entry.bind('<Return>', add_quick_task)
 
 root.mainloop()
