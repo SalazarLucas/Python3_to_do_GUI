7301330c35f51da51eae5a9eefac7aa56207335b
Lucas Salazar <lucassalazar741@gmail.com>
2020-08-02 15:18:49 -0300

    Update new_task_dialog.py

--- new_task_dialog.py
+++ new_task_dialog.py
@@ -47,17 +47,25 @@ class TaskEntry(Frame):
         self.entry = Entry(self, textvariable=self.new_task_string)
         self.entry.grid(column=1, row=2, sticky='nwse')
 
-        self.entry.bind('<FocusIn>', lambda *args: self.entry.delete(0, END))
-        self.entry.bind('<FocusOut>', lambda *args: self.entry.insert(0, 'Enter Task Here'))
+        self.entry.bind('<FocusIn>',
+                        lambda *args: self.entry.delete(0, END) if self.new_task_string.get() == 'Enter Task Here' else None)
+        self.entry.bind('<FocusOut>',
+                        lambda *args: self.entry.insert(0, 'Enter Task Here') if not self.entry.get() else None)
 
 
 class Date(Frame):
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
         self.grid_columnconfigure(1, weight=1)
-        self.date_string = StringVar()
+        self.date_string = StringVar(value='yyyy-mm-dd')
         self.label = Label(self, text='Due date').grid(column=1, row=1, sticky=W)
-        self.entry = Entry(self, textvariable=self.date_string).grid(column=1, row=2, sticky='nwse')
+        self.entry = Entry(self, textvariable=self.date_string)
+        self.entry.grid(column=1, row=2, sticky='nwse')
+
+        self.entry.bind('<FocusIn>',
+                        lambda *args: self.entry.delete(0, END) if self.date_string.get() == 'yyyy-mm-dd' else None)
+        self.entry.bind('<FocusOut>',
+                        lambda *args: self.entry.insert(0, 'yyyy-mm-dd') if not self.date_string.get() else None)
 
 
 class TaskLists(Frame):
@@ -83,4 +91,4 @@ class TaskWidget(Frame):
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
         self.checkbutton = Checkbutton(self, text=None, variable=self.info).grid(column=1, row=1, sticky=W)
-        self.date_label = Label(self, text=None).grid(column=1, row=2)
+        self.date_label = Label(self, text=None).grid(column=1, row=2, sticky=W)
