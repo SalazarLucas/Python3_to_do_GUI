11443b4204e89f49da21eb88ea433f01028b01a8
Lucas Salazar <lucassalazar741@gmail.com>
2020-08-03 15:17:07 -0300

    Update data_file_management.py

--- data_file_management.py
+++ data_file_management.py
@@ -1,2 +1,27 @@
-# Things to implement
-# - A system to control the storage of the task lists.
+import json
+
+
+class ToDoData(object):
+    try:
+        with open('lists.json', 'r') as file:
+            lists = json.load(file)
+        file.close()
+
+    except FileNotFoundError:
+        with open('lists.json', 'w') as file:
+            lists = {'All Lists': {},
+                     'Default': {},
+                     'Personal': {},
+                     'Shopping': {},
+                     'Wishlist': {},
+                     'Work': {},
+                     'Finished': {},
+                     'New List': {}}
+            json.dump(lists, file, indent=1)
+        file.close()
+
+    @classmethod
+    def set_lists(cls, obj):
+        with open('lists.json', 'w') as file:
+            json.dump(obj, file, indent=1)
+        file.close()
